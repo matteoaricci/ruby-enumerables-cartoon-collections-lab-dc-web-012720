6b1f429d9050e6e29336d857e33115bca05352e6
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 23:50:09 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(source)
+  source.each_with_index do |val, index|
+    index += 1
+    puts "#{index}. #{val}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(source)
+   source.collect do |n|
+     n.capitalize() + "!"
+   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(source)
+  source.any? do |n|
+    n.length > 4 
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(source)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  source.find do |n|
+    cheese_types.include?(n)
+  end 
 end
