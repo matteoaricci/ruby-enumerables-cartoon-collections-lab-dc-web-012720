faf56a82186851b6a58bde1f419819534edd7892
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 23:34:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(source)
    end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(source)
+  source.
 end
 
 def find_the_cheese# code an argument here
